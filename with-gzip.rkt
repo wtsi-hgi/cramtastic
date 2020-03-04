761f969aa8f78dbed261e34abf8995bad21477ee
Christopher Harrison <ch12@sanger.ac.uk>
2020-03-04 10:04:38 +0000

    Minor refactor and additional attribution

--- with-gzip.rkt
+++ with-gzip.rkt
@@ -28,49 +28,55 @@
 ; IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 ; POSSIBILITY OF SUCH DAMAGE.
 
-#lang racket
+; Modifications by Christopher Harrison <ch12@sanger.ac.uk>
+; Copyright (c) 2020 Genome Research Ltd.
+; Licensed under the terms of the GPLv3
 
-(require file/gzip 
+#lang racket/base
+
+(require racket/bool
+         racket/contract
+         file/gzip
          file/gunzip)
 
-(provide/contract 
- (with-gzip
-   (->* ((-> any))
-        (#:buffer-size (or/c false? exact-positive-integer?))
-        any)))
+(provide/contract
+  (with-gzip
+    (->* ((-> any))
+         (#:buffer-size (or/c false? exact-positive-integer?))
+         any)))
 
 (define (with-gzip thunk #:buffer-size [buffer-size #f])
   (define-values (pipe-from pipe-to) (make-pipe buffer-size))
   (dynamic-wind
-   void
-   (λ ()
-     (define t (thread (λ () (gzip-through-ports pipe-from (current-output-port) #f (current-seconds)))))
-     (parameterize ([current-output-port pipe-to])
-       (thunk))
-     (close-output-port pipe-to)
-     (thread-wait t))
-   (λ ()
-     (unless (port-closed? pipe-to) (close-output-port pipe-to))
-     (unless (port-closed? pipe-from) (close-input-port pipe-from))))
+    void
+    (lambda ()
+      (define t (thread (lambda () (gzip-through-ports pipe-from (current-output-port) #f (current-seconds)))))
+      (parameterize ([current-output-port pipe-to])
+        (thunk))
+      (close-output-port pipe-to)
+      (thread-wait t))
+    (lambda ()
+      (unless (port-closed? pipe-to) (close-output-port pipe-to))
+      (unless (port-closed? pipe-from) (close-input-port pipe-from))))
   (void))
 
-(provide/contract 
- (with-gunzip 
-   (->* ((-> any))
-        (#:buffer-size (or/c false? exact-positive-integer?))
-        any)))
-                  
+(provide/contract
+  (with-gunzip
+    (->* ((-> any))
+         (#:buffer-size (or/c false? exact-positive-integer?))
+         any)))
+
 (define (with-gunzip thunk #:buffer-size [buffer-size #f])
   (define-values (pipe-from pipe-to) (make-pipe buffer-size))
   (dynamic-wind
-   void
-   (λ ()
-     (thread 
-      (λ ()
-        (gunzip-through-ports (current-input-port) pipe-to)
-        (close-output-port pipe-to)))
-     (parameterize ([current-input-port pipe-from])
-       (thunk)))
-   (λ ()
-     (unless (port-closed? pipe-to) (close-output-port pipe-to))
-     (unless (port-closed? pipe-from) (close-input-port pipe-from)))))
+    void
+    (lambda ()
+      (thread
+        (lambda ()
+          (gunzip-through-ports (current-input-port) pipe-to)
+          (close-output-port pipe-to)))
+      (parameterize ([current-input-port pipe-from])
+        (thunk)))
+    (lambda ()
+      (unless (port-closed? pipe-to) (close-output-port pipe-to))
+      (unless (port-closed? pipe-from) (close-input-port pipe-from)))))
