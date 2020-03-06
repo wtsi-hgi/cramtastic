e4fbd56dc88d98ead0df8922927d9d6934316915
Christopher Harrison <ch12@sanger.ac.uk>
2020-03-06 10:45:07 +0000

    Factored out system call so it only happens once, on import

--- getent-group.rkt
+++ getent-group.rkt
@@ -19,18 +19,21 @@
   (group->gid   (-> string? exact-nonnegative-integer?)))
 
 
+;; getent system call
+(define getent
+  (let ((/path/to/getent (or (find-executable-path "getent")
+                             (error "getent not available"))))
+    (curry system* /path/to/getent)))
+
+
 ;; Wrapper for `getent group $@ 2>/dev/null`
 ; TODO Return a list of structs rather than raw strings
 (define (getent-group . groups)
-  (let* ((getent-path (or (find-executable-path "getent")
-                          (error "getent not available")))
-         (getent-exec (curry system* getent-path "group")))
-
-    (string-split
-      (parameterize ((current-error-port (open-output-nowhere)))
-        (with-output-to-string
-          (lambda () (apply getent-exec groups))))
-      "\n")))
+  (string-split
+    (parameterize ((current-error-port (open-output-nowhere)))
+      (with-output-to-string
+        (lambda () (apply getent "group" groups))))
+    "\n"))
 
 
 ;; Map group name to GID
