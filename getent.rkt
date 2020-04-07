dc7751b1836dec4d3dc75c0c98cfd8500e81222f
Christopher Harrison <ch12@sanger.ac.uk>
2020-04-07 16:26:59 +0100

    Generalise getent and implement passwd lookup

--- getent.rkt
+++ getent.rkt
@@ -1,4 +1,4 @@
-;;; getent group database interface
+;;; getent database interface
 
 ; Written by Christopher Harrison <ch12@sanger.ac.uk>
 ; Copyright (c) 2020 Genome Research Ltd.
@@ -17,49 +17,70 @@
 
 (provide
   (struct-out group)
+  (struct-out passwd)
+
   (contract-out
-    (getent-group (-> string? ... (listof group?)))
-    (group->gid   (-> string? exact-nonnegative-integer?))))
-
-
-;; getent system call
-(define getent
-  (let ((/path/to/getent (or (find-executable-path "getent")
-                             (error "getent not available"))))
-    (curry system* /path/to/getent)))
+    (getent-group  (-> string? ... (listof group?)))
+    (getent-passwd (-> string? ... (listof passwd?)))
+    (group->gid    (-> string? exact-nonnegative-integer?))
+    (gid->group    (-> exact-nonnegative-integer? string?))
+    (user->uid     (-> string? exact-nonnegative-integer?))
+    (uid->user     (-> exact-nonnegative-integer? string?))))
 
 
 ;; Group database record
 (struct group (name password gid users))
 
+;; Passwd database record
+(struct passwd (username password uid gid gecos home shell))
 
-;; Wrapper for `getent group $@ 2>/dev/null`
-(define (getent-group . groups)
-  (define output (open-output-string))
+;; Adaptor: Convert raw string input into the appropriate record struct
+;; TODO This only works by virtue of the records having different field
+;; counts for different databases; maybe make this a bit less hacky...
+(define (raw->record raw)
+  (match (string-split raw ":" #:trim? #f)
+    ((list name password gid users)
+     (group name
+            password
+            (string->number gid)
+            (string-split users ",")))
 
-  ; TODO Use getent's documented exit codes
-  (unless
-    (parameterize ((current-output-port output)
-                   (current-error-port  (open-output-nowhere)))
-      (apply getent "group" groups))
-
-    (error "getent call failed"))
-
-  ; Map each string record into group structure
-  (map
-    (lambda (record)
-      (match (string-split record ":" #:trim? #f)
-        ((list name password gid users) (group name
-                                               password
-                                               (string->number gid)
-                                               (string-split users ",")))))
-
-    (string-split (get-output-string output) "\n")))
+    ((list username password uid gid gecos home shell)
+     (passwd username
+             password
+             (string->number uid)
+             (string->number gid)
+             (string-split gecos ",")
+             (string->path home)
+             (string->path shell)))))
 
 
-;; Map group name to GID
-(define (group->gid group)
-  (group-gid (first (getent-group group))))
+;; Wrapper for `getent $DATABASE $@ 2>/dev/null`
+(define (getent database . keys)
+  (define /path/to/getent (or (find-executable-path "getent")
+                              (error "getent not available")))
+  (define get-entries
+    (parameterize ((current-error-port (open-output-nowhere)))
+      (curry system* /path/to/getent database)))
+
+  (define entries (with-output-to-string
+    (lambda ()
+      ; TODO Use getent's documented exit codes
+      (unless (apply get-entries keys)
+              (error "getent call failed")))))
+
+  (map raw->record (string-split entries "\n")))
+
+
+;; Exposed API
+(define (getent-group . groups) (apply getent "group" groups))
+(define (getent-passwd . users) (apply getent "passwd" users))
+
+(define (group->gid group) (group-gid (first (getent-group group))))
+(define (gid->group gid) (group-name (first (getent-group (number->string gid)))))
+
+(define (user->uid user) (passwd-uid (first (getent-passwd user))))
+(define (uid->user uid) (passwd-username (first (getent-passwd (number->string uid)))))
 
 
 (module+ test
@@ -71,4 +92,11 @@
     (check-equal? (group-name root-group) "root")
     (check-equal? (group-gid  root-group) 0))
 
-  (check-equal? (group->gid "root") 0))
+  (let* ((root-search (getent-passwd "root"))
+         (root-user   (first root-search)))
+    (check-equal? (length root-search) 1)
+    (check-equal? (passwd-username root-user) "root")
+    (check-equal? (passwd-uid      root-user) 0))
+
+  (check-equal? (gid->group (group->gid "root")) "root")
+  (check-equal? (uid->user  (user->uid  "root")) "root"))
